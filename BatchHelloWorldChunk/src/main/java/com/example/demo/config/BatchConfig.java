34b41a5fc0ebbee93ade7431c37074c4e83f427c
masatonara83 <masato.nara@rakus-partners.co.jp>
2022-04-13 15:46:22 +0900

    ログを出すlistenerを実装

--- BatchHelloWorldChunk/src/main/java/com/example/demo/config/BatchConfig.java
+++ BatchHelloWorldChunk/src/main/java/com/example/demo/config/BatchConfig.java
@@ -1,7 +1,9 @@
 package com.example.demo.config;
 
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepExecutionListener;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
@@ -37,6 +39,12 @@ public class BatchConfig {
 	@Autowired
 	private ItemWriter<String> writer;
 	
+	@Autowired
+	private JobExecutionListener jobExecutionListener;
+	
+	@Autowired
+	private StepExecutionListener stepExecutionListener;
+	
 	/*ChunkのStepの生成*/
 	@Bean
 	public Step chunkStep() {
@@ -45,6 +53,7 @@ public class BatchConfig {
 				.reader(reader)	//readerセット
 				.processor(processor)	//processorセット
 				.writer(writer)	//writerセット
+				.listener(stepExecutionListener) //StepListener
 				.build();	//stepの生成
 	}
 	
@@ -54,6 +63,7 @@ public class BatchConfig {
 		return jobBuilderFactory.get("HelloWorldChunkJob") //Builderの取得
 				.incrementer(new RunIdIncrementer())	//IDのインクリメント
 				.start(chunkStep())	//最初のStep
+				.listener(jobExecutionListener) //JobListener
 				.build(); //Jobの生成
 	}
 }
