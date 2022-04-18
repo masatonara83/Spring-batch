e0bd74c3065566bc8f65eedd54c21e0cb053f3ea
masatonara83 <masato.nara@rakus-partners.co.jp>
2022-04-18 17:55:10 +0900

    JDBCを使ったCSVファイル書き出しバッチを学習

---
+++ BatchCsvExport/src/main/java/com/example/demo/domain/model/Employee.java
@@ -0,0 +1,25 @@
+package com.example.demo.domain.model;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+	private Integer id;
+	private String name;
+	private Integer age;
+	private Integer gender;
+	private String genderString;
+	
+	/*性別の数値を文字列を変換*/
+	public void convertGenderIntToString() {
+		//数値に文字列を変換
+		if(gender == 1) {
+			genderString = "男性";
+		}else if (gender == 2) {
+			genderString = "女性";
+		} else {
+			String errorMsg = "Gneder is valid:" + gender;
+			throw new IllegalStateException(errorMsg);
+		}
+	}
+}
---
+++ BatchCsvExport/src/main/java/com/example/demo/listener/WriterListener.java
@@ -0,0 +1,34 @@
+package com.example.demo.listener;
+
+import java.util.List;
+
+import org.springframework.batch.core.ItemWriteListener;
+import org.springframework.stereotype.Component;
+
+import com.example.demo.domain.model.Employee;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class WriterListener implements ItemWriteListener<Employee> {
+	
+	@Override
+	public void beforeWrite(List<? extends Employee> items) {
+		log.debug("BeforeWrite Start");
+		
+	}
+
+	@Override
+	public void afterWrite(List<? extends Employee> items) {
+		log.debug("AfterWrite: count={}", items.size());
+		
+	}
+
+	@Override
+	public void onWriteError(Exception exception, List<? extends Employee> items) {
+		log.error("WriterError: errorMessage={}", exception.getMessage(), exception);
+		
+	}
+
+}
