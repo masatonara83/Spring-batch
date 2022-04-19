dfc5ad06a04c54e60c64cdaaed592c5f9b876ddf
masatonara83 <masato.nara@rakus-partners.co.jp>
2022-04-19 17:46:02 +0900

    JpaでのCSV書き出し

--- BatchCsvExport/src/main/java/com/example/demo/domain/model/Employee.java
+++ BatchCsvExport/src/main/java/com/example/demo/domain/model/Employee.java
@@ -1,13 +1,20 @@
 package com.example.demo.domain.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
 import lombok.Data;
 
 @Data
+@Entity
 public class Employee {
+	@Id
 	private Integer id;
 	private String name;
 	private Integer age;
 	private Integer gender;
+	@Transient
 	private String genderString;
 	
 	/*性別の数値を文字列を変換*/
