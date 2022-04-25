3e6585d5d81c71679c640a1173034cc8ae1feabf
masatonara83 <masato.nara@rakus-partners.co.jp>
2022-04-25 17:36:40 +0900

    taskletでのリトライ

---
+++ BatchRetryTasklet/src/main/java/com/example/demo/config/BatchConfig.java
@@ -0,0 +1,47 @@
+package com.example.demo.config;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@Configuration
+@EnableBatchProcessing
+
+public class BatchConfig {
+
+	/*StepbuilderのFactoryクラス*/
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+	
+	/*JobBuilderのFactoryクラス*/
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+	
+	@Autowired
+	private Tasklet retryTasklet;
+	
+	/*Step作成*/
+	@Bean
+	public Step retryTaskStep() {
+		return stepBuilderFactory.get("RetryTaskletStep")
+				.tasklet(retryTasklet).build();
+	}
+	
+	/*Jobの生成*/
+	@Bean
+	@Scheduled(fixedDelay = 5000)
+	public Job retryTaskletJob() throws Exception{
+		return jobBuilderFactory.get("retryTaskletjob")
+				.incrementer(new RunIdIncrementer())
+				.start(retryTaskStep())
+				.build();
+	}
+}
